278388f4b0db1c77ddb7ce9dd185317c01428b56
thiliapr <140580293+thiliapr@users.noreply.github.com>
2025-02-06 12:18:03 +0800

    new arg: avg of note

--- play.py
+++ play.py
@@ -12,6 +12,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("path", type=pathlib.Path)
     parser.add_argument("-s", "--strict", action="store_true", default=False, help="规范化数据时启用strict模式。因为需要尽量保留原来的音符时间差距，所以音符时间差距可能很大，导致有许多填充音符。")
+    parser.add_argument("-m", "--medium-mote", type=int, default=72, help="将音高的平均值调整到指定数值。范围: 0-127")
     args = parser.parse_args()
     return args
 
@@ -30,7 +31,7 @@ def main():
             print(mid.relative_to(args.path) if args.path.is_dir() else mid)
             notes = [(note, time) for _, note, time, _ in midi_to_notes(mido.MidiFile(mid, clip=True))]
             notes = norm_data(notes, TIME_PRECISION, 960, strict=args.strict)
-            notes_offest = max(0, int(64 - sum(note for note, _ in notes) / len(notes)))
+            notes_offest = max(0, int(args.medium_mote - sum(note for note, _ in notes) / len(notes)))
             for i, (note, time) in enumerate(notes):
                 note = note + notes_offest
                 if note > 127:
