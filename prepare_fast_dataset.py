7f49b2f79a152777096151cef1b471281eda9d53
thiliapr <140580293+thiliapr@users.noreply.github.com>
2025-09-21 18:12:19 +0800

    space

--- prepare_fast_dataset.py
+++ prepare_fast_dataset.py
@@ -86,7 +86,7 @@ def convert(
             # 合并窗口内的所有音高
             window_pitches = np.concatenate(active_pitches[time:time + frame_length])
 
-            # 计算归一化的音符数量（除以最大可能值128）
+            # 计算归一化的音符数量（除以最大可能值 128）
             note_counts[time] = len(window_pitches) / 128
 
             # 计算归一化的平均音高和音高范围
